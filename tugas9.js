4b0422010f783737cbccba8186e73a959c54b400
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-02 14:05:53 +0700

    Tugas 9 JS
    Tugas Niomic

---
+++ tugas9.js
@@ -0,0 +1,16 @@
+const tugas9 = () => {
+    var televisi = {
+        merk: 'Samsung',
+        ukuran: 32,
+        jenis: 'Smart TV',
+        harga: 2500000
+    }
+    console.log(televisi);
+    console.log('')
+    //for in
+    for(var x in televisi){
+        console.log(televisi[x])
+    }
+}
+
+tugas9();
